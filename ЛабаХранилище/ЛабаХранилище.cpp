8337ac3ca73c8271dec6c9760ff2ba78f4f5047d
Edik Daukaev <daukaev.eduard@mail.ru>
2020-11-14 11:48:28 +0500

    Изменение 2

--- ЛабаХранилище/ЛабаХранилище.cpp
+++ ЛабаХранилище/ЛабаХранилище.cpp
@@ -1,6 +1,5 @@
 ﻿
 #include <iostream>
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
@@ -11,28 +10,57 @@
 
 using namespace std;
 class MyStorage {
-        
+private :
+    Shape** objects;
+    int size;
+public:
+    MyStorage(int size) {
+        this->size = size;
+        objects = new Shape * [size];
+    }
+    void SetObject(int index, Shape* object) {
+        objects[index] = object;
+    }
+    Shape& GetObject(int index) {
+        return *objects[index];
+    }
+    void delete_obj(int index)
+    {
+
+    }
+    void add(Shape* object) {
+
+    }
+
+    int getCount() {
+        return size;
+    }
 };
 
 class Shape {
      Shape() {
 
     }
-
+     void show() {
+         cout << "Абстрактная фигура"<<endl;
+     }
     ~Shape() {
-        cout << "Фигура удалилась";
+        cout << "Фигура удалилась"<<endl;
     }
 
 };
 
 
-class Point : public Shape {
+class Point :public Shape {
 private: 
     int x,y;
 public:
     Point() {
         x = 0;
         y = 0;
+    }
+    void show() {
+        cout << "Точка с координатами( " << x << ", " << y << " )"<<endl;
     }
     ~Point() {
         cout << "Точка удалилась";
@@ -49,6 +77,9 @@ public :
         p1 = new Point;
         p2 = new Point;
     }
+    void show() {
+        cout << "Отрезок с точками : "; p1->show(); p2->show();
+    }
     ~Section() {
         delete p1;
         delete p2;
@@ -58,7 +89,10 @@ public :
 
 int main()
 {
-    cout << "Hello World!\n";
+    srand(time(NULL));
+    setlocale(LC_ALL, "Rus");
+    MyStorage storage(10);
+    for (int i = 0, i < storage.getCount(); i++) {
 
-
+    }
 }
