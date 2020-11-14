3ee807728d0f6c27263a6f3fdc439f64b1d5da5a
Edik Daukaev <daukaev.eduard@mail.ru>
2020-11-14 13:00:20 +0500

    Изменение 3

--- ЛабаХранилище/ЛабаХранилище.cpp
+++ ЛабаХранилище/ЛабаХранилище.cpp
@@ -9,6 +9,21 @@
 #include <ctime>
 
 using namespace std;
+
+class Shape {
+public:
+    Shape() {
+
+    }
+    virtual void show() {
+        cout <<endl<< "Абстрактная фигура" << endl;
+    }
+    ~Shape() {
+        cout << "Фигура удалилась" << endl;
+    }
+
+};
+
 class MyStorage {
 private :
     Shape** objects;
@@ -26,10 +41,19 @@ public:
     }
     void delete_obj(int index)
     {
-
+        objects[index]->~Shape();
     }
     void add(Shape* object) {
-
+        Shape** object1 = new Shape * [size + 1];
+        for (int i = 0; i < size; i++)
+            object1[i] = objects[i];
+        object1[size] = object;
+        size++;
+        delete objects;
+        objects = object1;
+        cout << "Добавили объект ";
+        object->show();
+        cout << "В память c индексом  " << size - 1 << endl;
     }
 
     int getCount() {
@@ -37,18 +61,7 @@ public:
     }
 };
 
-class Shape {
-     Shape() {
 
-    }
-     void show() {
-         cout << "Абстрактная фигура"<<endl;
-     }
-    ~Shape() {
-        cout << "Фигура удалилась"<<endl;
-    }
-
-};
 
 
 class Point :public Shape {
@@ -58,6 +71,10 @@ public:
     Point() {
         x = 0;
         y = 0;
+    }
+    Point(int x, int y) {
+        this->x = x;
+        this->y = y;
     }
     void show() {
         cout << "Точка с координатами( " << x << ", " << y << " )"<<endl;
@@ -74,11 +91,12 @@ private :
     int x1, x2, y1, y2;
 public :
     Section() {
-        p1 = new Point;
-        p2 = new Point;
+        p1 = new Point(rand()%10, (rand() % 10));
+        p2 = new Point(rand() % 10, (rand() % 10));
     }
     void show() {
-        cout << "Отрезок с точками : "; p1->show(); p2->show();
+        cout <<endl << "Отрезок с точками : "; p1->show(); p2->show();
+        cout << endl;
     }
     ~Section() {
         delete p1;
@@ -92,7 +110,17 @@ int main()
     srand(time(NULL));
     setlocale(LC_ALL, "Rus");
     MyStorage storage(10);
-    for (int i = 0, i < storage.getCount(); i++) {
-
+    for (int i = 0; i < storage.getCount(); i++) {
+        int a = rand() % 2 + 1;
+        if (a == 2)
+            storage.SetObject(i, new Point(rand()%10,rand()%10));
+        else
+            storage.SetObject(i, new Section());
     }
+    for (int i = 0; i < storage.getCount(); i++) {
+        storage.GetObject(i).show();
+    }
+
+
+    cout <<endl<< storage.getCount() << " - Это размер хранилища" << endl;
 }
